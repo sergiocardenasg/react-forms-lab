bae51bc1b13d1faa7dc1b5148615cf815a0286a4
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-23 14:13:47 -0500

    commit tree b957581ee546c9c4b2c8a6a789f26d89d7d9699a

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          name="message" 
+          id="message" 
+          onChange={this.handleChange}
+          value={this.state.message}/>
+        <p> Characters left: {this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
